15a98bd2a4a2630a29cbf61d4c84e37ae99f7ad0
EXL <exlmotodev@gmail.com>
2021-10-28 14:49:37 +0700

    Refactor code

--- driver/hellomoto.c
+++ driver/hellomoto.c
@@ -17,15 +17,17 @@
 #define PROC_MESSAGE_LENGTH 16
 #define PROC_ENTRY_FILENAME "hellomoto"
 
-static struct proc_dir_entry *g_ptr_proc_file = NULL;
+static struct proc_dir_entry *proc_file = NULL;
 
 static int hellomoto_read(char *buffer, char **buffer_location, off_t offset, int buffer_length, int *eof, void *data) {
 	if (offset <= 0) {
 		unsigned int message_length;
 		char message[PROC_MESSAGE_LENGTH] = { '\0' };
+
 		snprintf(message, PROC_MESSAGE_LENGTH, "0x%08X\n", gpio_signal_get_data_check(GPIO_SIGNAL_LENS_COVER));
 		message_length = strlen(message);
 		memcpy(buffer, message, message_length);
+
 		return message_length;
 	}
 	return 0;
@@ -35,46 +37,47 @@ static int hellomoto_write(struct file *filp, const char *buffer, unsigned long 
 	char message[PROC_MESSAGE_LENGTH] = { '\0' };
 	unsigned short keycode = 0x0000;
 	unsigned long buffer_size = count;
+
 	if (buffer_size > PROC_MESSAGE_LENGTH)
 		buffer_size = PROC_MESSAGE_LENGTH;
+
 	if (copy_from_user(message, buffer, buffer_size))
 		return -EFAULT;
+
 	keycode = simple_strtoul(message, NULL, 10);
-	printk(KERN_ALERT "hellomoto: Keycode: 0x%04X, Dec: %hu!\n", keycode, keycode);
+	printk(KERN_INFO "hellomoto: Keycode: 0x%04X, Dec: %hu!\n", keycode, keycode);
 	generate_key_event(keycode, KEYDOWN);
 	generate_key_event(keycode, KEYUP);
+
 	return buffer_size;
 }
 
 static int module_permission(struct inode *inode, int op, struct nameidata *foo) {
-	/* 2: write, 4: read */
-	if (op == 4 || op == 2)
+	if (op == 4 || op == 2) /* 2: write, 4: read */
 		return 0;
 	return -EACCES;
 }
 
-static struct inode_operations g_struct_inode_ops = {
-	.permission = module_permission,
-};
+static struct inode_operations iops = { .permission = module_permission };
 
 static int hellomoto_init(void) {
-	printk(KERN_ALERT "hellomoto: Hello, MotoMAGX modders!\n");
-	g_ptr_proc_file = create_proc_entry(PROC_ENTRY_FILENAME, 0644, NULL);
-	if (g_ptr_proc_file == NULL) {
+	printk(KERN_INFO "hellomoto: Hello, MotoMAGX modders!\n");
+	proc_file = create_proc_entry(PROC_ENTRY_FILENAME, 0644, NULL);
+	if (!proc_file) {
 		remove_proc_entry(PROC_ENTRY_FILENAME, &proc_root);
-		printk(KERN_ALERT "hellomoto: Could not initialize \"/proc/hellomoto\", sorry!\n");
+		printk(KERN_ERR "hellomoto: Could not initialize \"/proc/hellomoto\", sorry!\n");
 		return -ENOMEM;
 	}
-	g_ptr_proc_file->owner = THIS_MODULE;
-	g_ptr_proc_file->proc_iops = &g_struct_inode_ops;
-	g_ptr_proc_file->read_proc = hellomoto_read;
-	g_ptr_proc_file->write_proc = hellomoto_write;
+	proc_file->owner = THIS_MODULE;
+	proc_file->proc_iops = &iops;
+	proc_file->read_proc = hellomoto_read;
+	proc_file->write_proc = hellomoto_write;
 	return 0;
 }
 
 static void hellomoto_exit(void) {
+	printk(KERN_INFO "hellomoto: Goodbye, MotoMAGX modders!\n");
 	remove_proc_entry(PROC_ENTRY_FILENAME, &proc_root);
-	printk(KERN_ALERT "hellomoto: Goodbye, MotoMAGX modders!\n");
 }
 
 module_init(hellomoto_init);
